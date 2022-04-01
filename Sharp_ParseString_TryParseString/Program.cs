ef9dd373240994cebe5a9c221878f69c2612ec37
sofoyan gevork <aab8117@gmail.com>
2022-04-01 13:07:03 +0300

    Добавьте файлы проекта.

---
+++ Sharp_ParseString_TryParseString/Program.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Globalization;
+
+
+namespace Sharp_ParseString_TryParseString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+              string str = "5";
+
+            //парсим str в переменную a
+            int a = int.Parse(str);
+            */
+
+            /*string str = "5.9";
+
+            NumberFormatInfo numberFormatInfo = new NumberFormatInfo()
+            {
+                NumberDecimalSeparator = ".",
+            };
+
+            //парсим str в переменную a
+            double a = double.Parse(str, numberFormatInfo);*/
+
+            /*
+            string str = "1hgdhgf";
+            try
+            {
+                int a = Convert.ToInt32(str);
+                Console.WriteLine("Успешная конвертация");
+            }
+            catch (Exception)
+            {
+
+                Console.WriteLine("Ошибка при конвертации");
+            }
+            */
+
+            string str = "1 ора";
+            int a;
+
+            //метот принимает 2 параметра
+            //первый параметр это строка
+            //второй параметр переменная которая сохранит спарсенное значение наружу благодаря модификатору out
+            //Метот TryParse помимо спарсенной величины, возвращает булевое значение
+            //при успехе возвращает True
+            //при неудаяе False
+            bool result = int.TryParse(str, out a);
+
+            if(result)
+            {
+                Console.WriteLine($"Операция успешна, значение = {a}");
+            }
+            else
+            {
+                Console.WriteLine("Не удалось конвертировать!");
+            }
+        }
+    }
+}
